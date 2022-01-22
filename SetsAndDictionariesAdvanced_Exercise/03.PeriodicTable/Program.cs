fb8e485120ee3c7a90efa47e9aeae28cfa763443
anidz <96003907+anidz@users.noreply.github.com>
2022-01-22 18:20:47 +0200

    Sets and Dictionaries Advanced Ex

---
+++ SetsAndDictionariesAdvanced_Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = NewMethod();
+
+            int qty = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qty; i++)
+            {
+                string[] chemicals = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string chemical in chemicals) elements.Add(chemical);
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static SortedSet<string> NewMethod()
+        {
+            return new SortedSet<string>();
+        }
+    }
+    }
+

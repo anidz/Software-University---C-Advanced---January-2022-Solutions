c109b6d6528711ee953e593ef44735008be3c9bd
anidz <96003907+anidz@users.noreply.github.com>
2022-01-26 20:47:42 +0200

    Functional Programming Lab

---
+++ FunctionalProgramming_Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> evenChecker = x => x % 2 == 0;
+            Func<int, int> intOrderer = x => x;
+
+            int[] nums = Console.ReadLine()
+                .Split(", ").Select(int.Parse)
+                .Where(x => evenChecker(x))
+                .OrderBy(x => intOrderer(x)).ToArray();
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
+    
+
